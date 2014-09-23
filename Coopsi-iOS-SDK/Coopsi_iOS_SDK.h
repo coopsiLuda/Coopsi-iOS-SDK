792c21d51123aec58d2c079a5c2a2fc16288cfaf
fuxlud <fuxlud@gmail.com>
2014-09-23 16:36:20 +0300

    Initial Commit

---
+++ Coopsi-iOS-SDK/Coopsi_iOS_SDK.h
@@ -0,0 +1,13 @@
+//
+//  Coopsi_iOS_SDK.h
+//  Coopsi-iOS-SDK
+//
+//  Created by Luda Fux on 9/23/14.
+//  Copyright (c) 2014 Coopsi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Coopsi_iOS_SDK : NSObject
+
+@end
---
+++ Coopsi-iOS-SDK/Coopsi_iOS_SDK.m
@@ -0,0 +1,13 @@
+//
+//  Coopsi_iOS_SDK.m
+//  Coopsi-iOS-SDK
+//
+//  Created by Luda Fux on 9/23/14.
+//  Copyright (c) 2014 Coopsi. All rights reserved.
+//
+
+#import "Coopsi_iOS_SDK.h"
+
+@implementation Coopsi_iOS_SDK
+
+@end
